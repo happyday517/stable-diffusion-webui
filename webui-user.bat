a640c22f3efd183ff863f1d8b057bfe8151eb21e
FreeStyle <happyday517@users.noreply.github.com>
2023-06-19 14:36:12 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= --xformers
+set https_proxy=http://127.0.0.1:7890
+set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:6144
+set no_proxy=localhost,127.0.0.1,::1,mirrors.aliyun.com,mirrors.cloud.tencent.com
 call webui.bat
